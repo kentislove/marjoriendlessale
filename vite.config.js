b9825d369545c908c13948675a11f04265bd3a11
kentislove <newlife691027@gmail.com>
2025-11-23 14:36:47 +0800

    恢復 base 路徑為 /marjoriendlessale/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 根路徑，用於 kentislove.github.io
+  base: '/marjoriendlessale/', // GitHub Pages repository 名稱
 })
